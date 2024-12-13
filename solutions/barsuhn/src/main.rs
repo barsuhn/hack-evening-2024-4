3119bf612d72c092fd6ad3f1aa3e319bb4e27b30
Bernd Barsuhn <bernd.barsuhn@me.com>
2024-12-13 02:37:13 +0100

    Somewhat optimized

--- solutions/barsuhn/src/main.rs
+++ solutions/barsuhn/src/main.rs
@@ -4,16 +4,18 @@ use std::io::{BufRead, BufReader};
 use std::process::exit;
 use clap::Parser;
 use anyhow::{anyhow, Result};
-use crate::command::Args;
-use crate::weather::Weather;
+use crate::weather::{Measurement, Weather};
 
-mod command;
 mod weather;
+
+#[derive(Parser)]
+pub struct Args {
+    pub filename: String
+}
 
 fn main() {
     let args = Args::parse();
-
-    let map = match read_weather(&args.filename) {
+    let map = match read_measurements(&args.filename) {
         Ok(map) => map,
         Err(e) => {
             eprintln!("Error: {}", e.to_string());
@@ -21,50 +23,48 @@ fn main() {
         }
     };
 
-    let mut keys = map.keys().collect::<Vec<_>>();
+    let mut items = map.into_iter()
+        .map(|(name,measurement)| Weather::new(name, measurement))
+        .collect::<Vec<_>>();
 
-    keys.sort();
+    items.sort_by(|i, j| i.name.cmp(&j.name));
 
-    println!("{}", '{');
-    let n = keys.len();
-    let mut c = 0_usize;
-
-    for key in keys {
-        let item = map.get(key).unwrap();
-        let summary = item.summarize(key);
-
-        c += 1;
-        if c != n {
-            println!("    {},", summary);
-        } else {
-            println!("    {}", summary);
-        }
+    match print_solution(&items) {
+        Ok(()) => (),
+        Err(e) => eprintln!("Error: {}", e.to_string()),
     }
-    print!("{}", '}');
 }
 
-fn read_weather(filename: &str) -> Result<HashMap<String,Weather>> {
-    let mut map = HashMap::<String,Weather>::new();
+fn read_measurements(filename: &str) -> Result<HashMap<String,Measurement>> {
+    let mut map = HashMap::<String,Measurement>::new();
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines().flatten() {
-        let parts = line.split(";").collect::<Vec<_>>();
-
-        if parts.len() != 2 {
-            return Err(anyhow!("Invalid line format {}", line));
-        }
-
-        let (name,value) = (parts[0], parts[1].parse::<f64>()?);
-
-        let mut item = match map.get(name) {
-            Some(item) => item.clone(),
-            None => Weather::new()
+        let mut parts = line.split(";");
+        let (name ,value) = match (parts.next(), parts.next()) {
+            (Some(name), Some(value)) => (name, value.parse::<f64>()?),
+            _ => continue
         };
 
-        item.add(value);
-        map.insert(name.to_string(), item);
+        match map.get_mut(name) {
+            Some(item) => item.add(value),
+            _ => { map.insert(name.to_string(), Measurement::new(value)); }
+        };
     }
 
     Ok(map)
+}
+
+fn print_solution(items: &Vec<Weather>) -> Result<()> {
+    let first = items.first().ok_or(anyhow!("empty collection"))?.summarize();
+
+    println!("{}", '{');
+    print!("    {}", first);
+    for summary in items.iter().skip(1).map(|item| item.summarize()) {
+        print!(",\n    {}", summary);
+    }
+    print!("\n{}", '}');
+
+    Ok(())
 }
